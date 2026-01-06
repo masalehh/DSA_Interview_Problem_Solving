c4b327157fd12a4f71af361fd035f8425ef7120b
masalehh <ma.saleh007@gmail.com>
2026-01-06 15:56:19 +0600

    733 flood fill solved by own effort

---
+++ Graph/733_lc_Flood_Fill.py
@@ -0,0 +1,45 @@
+from typing import List, Set, Tuple
+
+
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        visited: Tuple[int, int] = set()
+
+        rows, cols = len(image), len(image[0])
+        starting_coordinate_value = image[sr][sc]
+
+        def flood_fill_helper(r, c):
+            if (
+                r < 0 or r >= rows or
+                c < 0 or c >= cols or
+                (r, c) in visited or
+                image[r][c] != starting_coordinate_value
+            ):
+                return
+
+            visited.add((r, c))
+            image[r][c] = color
+
+            flood_fill_helper(r+1, c)
+            flood_fill_helper(r-1, c)
+            flood_fill_helper(r, c+1)
+            flood_fill_helper(r, c-1)
+
+        flood_fill_helper(sr, sc)
+        return image
+
+
+images = [[1, 1, 1], [1, 1, 0], [1, 0, 1]]
+r = 1
+c = 1
+colors = 2
+image2 = [[0,0,0],[0,0,0]]
+image3 = [[0,0,0],[0,0,0]]
+sol = Solution()
+print(sol.floodFill(image3, 0, 0, 2))
+
+"""image = [[1, 1, 1], 
+         [1, 1, 0], 
+         [1, 0, 1]]
+=== Accepted in leetcode ===
+"""
