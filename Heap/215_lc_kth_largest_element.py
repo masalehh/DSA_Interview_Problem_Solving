61470db0bf092defe50003d90a4e837ef846d460
masalehh <ma.saleh007@gmail.com>
2026-02-06 10:33:16 +0600

    solved leetcode 215 problem by own effort within 10 minutes

---
+++ Heap/215_lc_kth_largest_element.py
@@ -0,0 +1,14 @@
+# solve it by own way within 10 minutes using min heap
+
+import heapq
+from typing import List
+
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heapq.heapify(nums)
+        n = len(nums) - 1
+        while n >= k:
+            heapq.heappop(nums)
+            n -= 1
+        return nums[0]
