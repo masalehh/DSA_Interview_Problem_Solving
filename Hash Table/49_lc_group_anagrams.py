b4badcb444b7973d72393258b32e1473769536a4
masalehh <ma.saleh007@gmail.com>
2026-04-17 19:36:16 +0600

    solved by own effort

---
+++ Hash Table/49_lc_group_anagrams.py
@@ -0,0 +1,13 @@
+from typing import List
+from collections import Counter, defaultdict
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result = defaultdict(list)
+
+        for word in strs:
+            sorted_word = "".join(sorted(word))
+            result[sorted_word].append(word)
+        return list(result.values()) 
+
