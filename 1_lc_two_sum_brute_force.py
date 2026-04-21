a1c692b44fdd7efe212aefc7ad40a43a5ad47812
masalehh <ma.saleh007@gmail.com>
2026-04-21 18:32:58 +0600

    udpate

--- 1_lc_two_sum_brute_force.py
+++
@@ -1,6 +0,0 @@
-def two_sum(nums, target_sum):
-    n = len(nums)
-    for i in range(n):
-        for j in range(i+1, n):
-            if nums[i] + nums[j] == target_sum:
-                return [i, j]
