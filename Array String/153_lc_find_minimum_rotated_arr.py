1a0ce8a07dfa0ed688b9197e26927aee7670fe6f
masalehh <ma.saleh007@gmail.com>
2026-04-28 17:44:14 +0600

    medium problem leetcode solved

---
+++ Array String/153_lc_find_minimum_rotated_arr.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        left, right = 0, len(nums) - 1
+        ans = nums[0]
+
+        while left <= right:
+            mid = (left + right) // 2
+            ans = min(nums[mid], ans)
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return ans
