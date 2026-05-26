a5f1b370609f7cbd1d034145a6a6a6ef1fa61bd8
masalehh <ma.saleh007@gmail.com>
2026-05-26 20:22:21 +0600

    solved this within a minute

---
+++ Array String/287_lc_find_duplicate.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        visited = set()
+        for num in nums:
+            if num not in visited:
+                visited.add(num)
+            else:
+                return num
