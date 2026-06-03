899b8ed3525049486d4696b5c94df04557c8d5e5
masalehh <ma.saleh007@gmail.com>
2026-06-03 08:20:47 +0600

    100 leetcode check two tree is same with value or not

---
+++ Tree/100_lc_same_tree.py
@@ -0,0 +1,37 @@
+from typing import Optional
+# Definition for a binary tree node.
+# class TreeNode:
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        queue_p = deque([p])
+        queue_q = deque([q])
+
+        while queue_p and queue_q:
+            for _ in range(len(queue_p)):
+                node_p = queue_p.popleft()
+                node_q = queue_q.popleft()
+                if node_p is None and node_q is None:
+                    continue
+
+                if node_p is None or node_q is None or node_p.val != node_q.val:
+                    return False
+
+                queue_p.append(node_p.left)
+
+                queue_p.append(node_p.right)
+
+                queue_q.append(node_q.left)
+
+                queue_q.append(node_q.right)
+
+        return True
