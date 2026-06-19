4c45b62cad5e84db86e49fb0886959e432005366
masalehh <ma.saleh007@gmail.com>
2026-06-20 04:16:48 +0600

    solved with O(n) time and space complexity

---
+++ Tree/98_validate_bst.py
@@ -0,0 +1,37 @@
+from typing import List, Optional
+from math import inf
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def inorder(node):
+            if node is None:
+                return True
+
+            if not inorder(node.left):
+                return False
+
+            nonlocal prev
+            if prev >= node.val:
+                return False
+            prev = node.val
+
+            return inorder(node.right)
+
+        prev = -inf
+
+        return inorder(root)
+
+
+"""
+Time Complexity: O(n)
+Space Complexity: O(n)
+"""
