688b3211d77b5b05b62d5afd1a2fd5f262794bb8
masalehh <ma.saleh007@gmail.com>
2026-06-27 16:42:35 +0600

    solved using two pointers

---
+++ Array String/121_buy_sell_stock_two_pointer.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        left, right = 0, 1
+
+        while right < len(prices):
+            if prices[left] < prices[right]:
+                max_profit = max(prices[right] - prices[left], max_profit)
+            else:
+                left = right
+            right += 1
+        return max_profit
