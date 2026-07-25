cf0fcce4521db3a72fb21d8d52420fd812492bc8
masalehh <ma.saleh007@gmail.com>
2026-07-25 06:28:45 +0600

    update

--- Tree/226_invert_binary_tree_myself.py
+++ Tree/226_invert_binary_tree_myself.py
@@ -7,6 +7,8 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
+
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
